1867a95cf3c10643773351f4ebc1ebc58d077531
Shoghi Cervantes <shoghicp@gmail.com>
2013-11-29 00:17:22 +0100

    Removed unused things

--- proxy/src/utils/pthreads.php
+++ proxy/src/utils/pthreads.php
@@ -20,9 +20,6 @@
  *
 */
 
-define("ASYNC_CURL_GET", 1);
-define("ASYNC_CURL_POST", 2);
-
 class StackableArray extends Stackable{
 	public function __construct(){
 		foreach(func_get_args() as $n => $value){
@@ -34,100 +31,8 @@ class StackableArray extends Stackable{
 			}
 		}
 	}
+	
+	public function __destruct(){}
+	
 	public function run(){}
-}
-
-class AsyncMultipleQueue extends Thread{
-	public $input;
-	public $output;
-	public $stop;
-	public function __construct(){
-		$this->input = "";
-		$this->output = "";
-		$this->stop = false;
-		$this->start();
-	}
-	
-	private function get($len){
-		$str = "";
-		if($len <= 0){
-			return $len;
-		}
-		$offset = 0;
-		while(!isset($str{$len - 1})){
-			if(isset($this->input{$offset})){
-				$str .= $this->input{$offset};
-				++$offset;
-			}
-		}
-		$this->input = (string) substr($this->input, $offset);
-		return $str;
-	}
-	
-	public function run(){
-		while($this->stop === false){
-			if(isset($this->input{5})){ //len 6 min
-				$rID = Utils::readInt($this->get(4));
-				switch(Utils::readShort($this->get(2), false)){
-					case ASYNC_CURL_GET:
-						$url = $this->get(Utils::readShort($this->get(2), false));
-						$timeout = Utils::readShort($this->get(2));
-						
-						$res = (string) Utils::curl_get($url, $timeout);
-						$this->lock();
-						$this->output .= Utils::writeInt($rID).Utils::writeShort(ASYNC_CURL_GET).Utils::writeInt(strlen($res)).$res;
-						$this->unlock();
-						break;
-					case ASYNC_CURL_POST:
-						$url = $this->get(Utils::readShort($this->get(2), false));
-						$timeout = Utils::readShort($this->get(2));
-						$cnt = Utils::readShort($this->get(2), false);
-						$d = array();
-						for($c = 0; $c < $cnt; ++$c){
-							$key = $this->get(Utils::readShort($this->get(2), false));
-							$d[$key] = $this->get(Utils::readInt($this->get(4), false));
-						}
-						$res = (string) Utils::curl_post($url, $d, $timeout);
-						$this->lock();
-						$this->output .= Utils::writeInt($rID).Utils::writeShort(ASYNC_CURL_POST).Utils::writeInt(strlen($res)).$res;
-						$this->unlock();
-						break;
-				}
-			}
-			usleep(10000);
-		}
-	}
-}
-
-class Async extends Thread{
-	public function __construct($method, $params = array()){
-		$this->method = $method;
-		$this->params = $params;
-		$this->result = null;
-		$this->joined = false;
-	}
-
-	public function run(){
-		if(($this->result=call_user_func_array($this->method, $this->params))){
-			return true;
-		}else{
-			return false;
-		}
-	}
-
-	public static function call($method, $params = array()){
-		$thread = new Async($method, $params);
-		if($thread->start()){
-			return $thread;
-		}
-	}
-
-	public function __toString(){
-		if(!$this->joined){
-			$this->joined = true;
-			$this->join();
-		}
-
-		return $this->result;
-	}
 }
